b5e09e5a04407d84b2f2255655683f99413c8921
Crimson-streaming <aamarrelam@icloud.com>
2025-01-16 20:55:40 +0100

    index update

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ async function chargerCategorie(categorie, conteneur) {
                     <a href="${film.emplacement}" title="${film.nom}">
                         <div class="video-img">
                             <span class="video-item-content">${film.nom}</span>
-                            <img src="${film.affiche}" alt="${film.nom}" title="${film.nom}">
+                            <img src="${film.affiche}" alt="${film.nom}" title="${film.nom}" loading="lazy">
                         </div>
                     </a>
                 </div>
