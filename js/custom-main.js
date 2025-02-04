c45289c1dd0e694b73f2ab58a51a71502728dffe
Crimson-streaming <aamarrelam@icloud.com>
2025-02-04 21:19:54 +0100

    Resolved bug film

--- js/custom-main.js
+++ js/custom-main.js
@@ -1,15 +1,30 @@
-
-
-    e(".splide").length>0&&new Splide(".splide", {
-        autoplay:!0,rewind:!0,focus:"center",autoWidth:!0,pauseOnHover:!1,pauseOnFocus:!1,pagination:!1,type:"loop",gap:"1em",padding: {
-            right:"8rem",left:"8rem"
-        },
-        breakpoints: {
-            767: {
+!function(e) {
+    "use strict";
+    e(window).on("load", (function() {
+        if (e(".splide").length > 0) {
+            new Splide(".splide", {
+                autoplay: !0,
+                rewind: !0,
+                focus: "center",
+                autoWidth: !0,
+                pauseOnHover: !1,
+                pauseOnFocus: !1,
+                pagination: !1,
+                type: "loop",
+                gap: "1em",
                 padding: {
-                    right:"0rem",left:"0rem"
+                    right: "8rem",
+                    left: "8rem"
+                },
+                breakpoints: {
+                    767: {
+                        padding: {
+                            right: "0rem",
+                            left: "0rem"
+                        }
+                    }
                 }
-            }
+            }).mount();
         }
-    })
-   
+    }));
+}(jQuery);
