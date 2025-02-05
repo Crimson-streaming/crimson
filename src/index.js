0a1a2db383d1b0aa2d40e496ac03426643a42605
Crimson-streaming <aamarrelam@icloud.com>
2025-02-05 23:57:27 +0100

    Retirer le dossier src du suivi

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './player.js';
-import '../js/search.js';
-import './trailer.js';
-import './watchlist.js';
-import './suggestions.js';
-import './recentely-watched.js';
-import './webview.js';
-
-console.log("Tous les scripts sont chargés !");
