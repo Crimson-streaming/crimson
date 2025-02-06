1ba75a96b5bce0dde535378b14d049b829c1f59a
Crimson-streaming <aamarrelam@icloud.com>
2025-02-06 20:50:41 +0100

    test film

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './player.js';
+import './trailer.js';
+import './watchlist.js';
+import './suggestions.js';
+import './recentely-watched.js';
+import './webview.js';
+
+console.log("Tous les scripts sont chargés !");
